81a97dd6d213d5c49d1ab7dcc4167be4900dd160
KUSUMAPRIYAPULAVARTY <70574378+KUSUMAPRIYAPULAVARTY@users.noreply.github.com>
2020-09-17 17:32:51 +0530

    Update plot.py

--- codes/plot.py
+++ codes/plot.py
@@ -1,18 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from coeffs import * 
+
+
+M = np.array([-12,1]) #a point satisfying equation1
+N = np.array([3,-4])  # another point satisfying equation1
+O = np.array([1,5]) #a point satisfying equation2
+P = np.array([-3,-7]) #another point satisfying equation2
+#Generating all lines
+x_AB = line_gen(M,N)
+x_CD = line_gen(O,P)
 
 #Plotting all lines
-x = np.linspace(-10, 10, 80)
-y1=(-x-9)/3
-y2 =3*x+2
-plt.plot(x,y1, label ='x+3y+9=0')
-plt.plot(x,y2, label = '-3x+y-2=0')
+plt.plot(x_AB[0,:],x_AB[1,:],label='$x+3y+9=0$')
+plt.plot(x_CD[0,:],x_CD[1,:],label='$-3x+y-2=0$')
 plt.plot(-1.5, -2.5, 'ro', label = 'intersection point(-1.5,-2.5)')
-plt.xlim(-10,10)
-plt.ylim(-10,10)
-plt.legend()
+
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.grid() 
-#plt.savefig('hw4plot.png')
+plt.legend()
+plt.grid() # minor
+plt.axis('equal')
 plt.show()
